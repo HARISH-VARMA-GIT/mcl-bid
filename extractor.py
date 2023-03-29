ecf8a3480ad1e73b0997d90619f96d57c1943b81
ayan <mukherjeeayan0813@gmail.com>
2023-03-29 22:47:13 +0530

    initial commit

---
+++ extractor.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 21:19:14 2023
+
+@author: mukhe
+"""
+
+import mcl_ca as mclc
+import mcl_gstin as mclg 
+import mcl_legal as mcll
+import mcl_pan as mclp
+
+def extract_pan(filepath):
+    return mclp.getPAN(filepath)
+
+def extract_gstin(filepath):
+    return mclg.getGSTIN(filepath)
+
+def extract_info_from_ca(filepath):
+    return mclc.extract_from_CA(filepath)
+
+def extract_info_from_legal(filepath):
+    return mcll.extract_from_legal(filepath)
+
+
---
+++ mcl_utils.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 23:31:32 2023
+
+@author: mukhe
+"""
+
+import fitz
+from PIL import Image
+import pytesseract
+import os
+import openai
+
+openai.api_key = "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"
+pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
+def extract_text(filepath, startpg=1, endpg=None):
+  file = fitz.open(filepath)
+  filename = filepath.split("\\")[-1].split(".")[0]
+  fileloc = "\\".join(filepath.split("\\")[:-1])
+  if endpg == None:
+      endpg = len(file)
+  text = ""
+  images = []
+  for i in range(startpg-1, endpg):
+      pix = file[i].get_pixmap()
+      imgloc = fileloc + "\\" + filename + str(i) + ".jpg"
+      images.append(imgloc)
+      pix.save(imgloc, "JPEG")
+  for img in images:
+      page_text = pytesseract.image_to_string(Image.open(img))
+      text += " -- " + page_text
+      os.remove(img)
+  return text
+
+def get_answer(context, question):
+  prompt = context + " What is the" + question + " in the above text? Say just the" + question + " in quotation marks or parentheses."
+  return openai.ChatCompletion.create(
+      messages=[{
+          "role": "assistant",
+          "content": prompt
+      }],
+      temperature=0,
+      max_tokens=300,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0,
+      model="gpt-3.5-turbo"
+  )["choices"][0]["message"]["content"]
